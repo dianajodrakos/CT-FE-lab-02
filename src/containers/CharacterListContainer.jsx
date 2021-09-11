2e419cbfae3193b3189e98e13c6c85dff73cfa28
DJ Drakos <dianajodrakos@gmail.com>
2021-09-10 17:59:49 -0700

    updated character list container component test to pass

--- src/containers/CharacterListContainer.jsx
+++ src/containers/CharacterListContainer.jsx
@@ -16,7 +16,8 @@ export default class CharacterListContainer extends Component {
           {
             villagers,
             loading: false 
-          })
+          }
+        )
       );
   }
 
--- src/containers/CharacterListContainer.test.jsx
+++ src/containers/CharacterListContainer.test.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router';
 import { render, screen } from '@testing-library/react';
-import CharacterListContainer from './CharacterListContainer';
+import CharacterListContainer from './CharacterListContainer.jsx';
+
 
 describe('Animal Crossing Character List Container', () => {
   it('renders a list of villagers', async () => {
-    render(<CharacterListContainer />);
-
+    render(
+      <MemoryRouter>
+        <CharacterListContainer />
+      </MemoryRouter>
+    );
+    
     screen.getByText('loading...');
 
-    const ul = await screen.findByRole('list', { name: 'villagers' });
+    const ul = await screen.findByRole(
+      'list', 
+      { name: 'villagers' }, 
+      { timeout: 10000 }
+    );
 
     expect(ul).toMatchSnapshot();
     
