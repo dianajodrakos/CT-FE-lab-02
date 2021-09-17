b05dbe6ad629df284f7cd57ac3cbdab7ee39d949
DJ Drakos <dianajodrakos@gmail.com>
2021-09-17 10:35:54 -0700

    refactor character detail container test to app test suite

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 import { render, screen, cleanup } from '@testing-library/react';
 import { rest } from 'msw';
@@ -5,6 +6,7 @@ import { setupServer } from 'msw/node';
 import { act } from 'react-dom/test-utils';
 import { MemoryRouter } from 'react-router';
 import listData from '../../fixtures/listData.json';
+import detailData from '../../fixtures/detailData.json';
 import App from './App';
 
 const server = setupServer(
@@ -12,6 +14,12 @@ const server = setupServer(
     return res(
       ctx.status(200),
       ctx.json(listData)
+    );
+  }),
+  rest.get('https://ac-vill.herokuapp.com/villagers/5f5fb4bbbfd05c2aed82e462', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json(detailData)
     );
   })
 );
@@ -49,5 +57,21 @@ describe('Animal Crossing App', () => {
     );
 
     expect(ul).toMatchSnapshot();
+  });
+
+  it('renders a single villager', async () => {
+    act(() => {
+      render(
+        <MemoryRouter initialEntries={['/Agnes']}>
+          <App />
+        </MemoryRouter>
+      );
+    });
+
+    screen.getByAltText('loading');
+
+    const h2 = await screen.findByRole('heading');
+
+    expect(h2).toMatchSnapshot();
   });
 });
