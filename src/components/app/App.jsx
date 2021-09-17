d7e52861b63aac8eb9cba0c86cf9cddb0ece808c
DJ Drakos <dianajodrakos@gmail.com>
2021-09-17 09:42:14 -0700

    add header

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,19 +1,19 @@
+/* eslint-disable max-len */
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from 'react-router-dom';
+import {  Route,  Switch } from 'react-router-dom';
 import CharacterListContainer from '../../containers/CharacterListContainer';
 import CharacterDetailContainer from '../../containers/CharacterDetailContainer';
+import Header from '../header/Header';
 
 export default function App() {
   return ( 
-    <Router>
+    <>
+      <Header />
       <Switch>
         <Route exact path="/" component={CharacterListContainer} />
         <Route exact path="/:name" component={CharacterDetailContainer} />
       </Switch>
-    </Router>);
+    </>
+  );
 }
 
