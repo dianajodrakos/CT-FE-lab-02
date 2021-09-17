fc31528c0a2c94715b0701a81ad1de628cc44b91
DJ Drakos <dianajodrakos@gmail.com>
2021-09-17 10:23:43 -0700

    refactor character list container test to app test suite

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-import App from './App';
+import { render, screen, cleanup } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { act } from 'react-dom/test-utils';
 import { MemoryRouter } from 'react-router';
+import listData from '../../fixtures/listData.json';
+import App from './App';
+
+const server = setupServer(
+  rest.get('https://ac-vill.herokuapp.com/villagers/', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json(listData)
+    );
+  })
+);
 
 describe('App component', () => {
   afterEach(() => cleanup());
@@ -12,5 +25,29 @@ describe('App component', () => {
       </MemoryRouter>
     );
     expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+describe('Animal Crossing App', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
+  it('renders a list of villagers', async () => {
+    act(() => {
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      );
+    });
+
+    screen.getByAltText('loading');
+
+    const ul = await screen.findByRole(
+      'list',
+      { name: 'villagers' },
+    );
+
+    expect(ul).toMatchSnapshot();
   });
 });
